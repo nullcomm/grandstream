db44409539ddf38bef4c385fcd20146012f48dd6
Cody Null <cody.b.null@gmail.com>
2016-07-08 06:50:41 -0500

    Fixed reboot() command for older firmware devices changed to shell_exec()

--- grandstream/ht70x/phone.php
+++ grandstream/ht70x/phone.php
@@ -40,38 +40,44 @@ class endpoint_grandstream_ht70x_phone extends endpoint_grandstream_base {
         }
 
     }
-
+    
     function reboot() {
         if (($this->engine == "asterisk") AND ($this->system == "unix")) {
-            exec($this->engine_location . " -rx 'sip show peers like " . $this->settings['line'][0]['username'] . "'", $output);
-            if (preg_match("/\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/", $output[1], $matches)) {
-                $ip = $matches[0];
-                $pass = (isset($this->options['admin_pass']) ? $this->options['admin_pass'] : 'admin');
+            $result = '';
+            $mac = $this->settings['mac'];
+            while (strlen($mac) > 0) {
+                $sub = substr($mac, 0, 2);
+                $result .= $sub . ':';
+                $mac = substr($mac, 2, strlen($mac));
+            }
+            // remove trailing colon
+            $result = strtolower(substr($result, 0, strlen($result) - 1));
+            $ip = trim((shell_exec("/usr/sbin/arp -n | grep " . $result . " | awk '{print $1}'")));
+            $pass = (isset($this->options['admin_pass']) ? $this->options['admin_pass'] : 'admin');
 
-                if (function_exists('curl_init')) {
-                    $ckfile = tempnam($this->sys_get_temp_dir(), "GSCURLCOOKIE");
-                    $ch = curl_init('http://' . $ip . '/cgi-bin/dologin');
-                    curl_setopt($ch, CURLOPT_COOKIEJAR, $ckfile);
-                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                    curl_setopt($ch, CURLOPT_POST, true);
+            if (function_exists('curl_init')) {
+                $ckfile = tempnam($this->sys_get_temp_dir(), "GSCURLCOOKIE");
+                $ch = curl_init('http://' . $ip . '/cgi-bin/dologin');
+                curl_setopt($ch, CURLOPT_COOKIEJAR, $ckfile);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($ch, CURLOPT_POST, true);
 
-                    $data = array(
-                        'P2' => $pass,
-                        'Login' => 'Login',
-                        'gnkey' => '0b82'
-                    );
+                $data = array(
+                    'P2' => $pass,
+                    'Login' => 'Login',
+                    'gnkey' => '0b82'
+                );
+                
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+                $output = curl_exec($ch);
+                $info = curl_getinfo($ch);
+                curl_close($ch);
 
-                    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-                    $output = curl_exec($ch);
-                    $info = curl_getinfo($ch);
-                    curl_close($ch);
-
-                    $ch = curl_init("http://" . $ip . "/cgi-bin/rs");
-                    curl_setopt($ch, CURLOPT_COOKIEFILE, $ckfile);
-                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                    $output = curl_exec($ch);
-                    curl_close($ch);
-                }
+                $ch = curl_init("http://" . $ip . "/cgi-bin/rs");
+                curl_setopt($ch, CURLOPT_COOKIEFILE, $ckfile);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                $output = curl_exec($ch);
+                curl_close($ch);
             }
         }
     }
